d70f740066fe863dab6c7673797b5ce617db20c0
Mcrymbo <croco2023@gmail.com>
2023-08-10 13:36:50 +0000

    handle arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+    length = len(argv) - 1
+
+    if length == 0:
+        print("{} arguments.".format(length))
+    elif length == 1:
+        print("{} arguments:".format(length))
+    else:
+        print("{} arguments:".format(length))
+    if length >= 1:
+        length = 0
+        for arg in argv:
+            if length != 0:
+                print("{}: {}".format(length, arg))
+            length += 1
