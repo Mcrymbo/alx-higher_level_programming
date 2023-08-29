741b003aa4ba1a943e72a6c6327f4164c5ec6bcb
Mcrymbo <croco2023@gmail.com>
2023-08-29 13:25:05 +0000

    area square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+class Square:
+    """Class Square determines the square of based on size
+    """
+    def __init__(self, size=0):
+        """ Initialize method to initialize the size of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Class Method calculates and return square of the object
+        """
+        return (self.__size ** 2)
