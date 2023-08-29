ce40f4a2731475d0e3ed5b3446d7393b85856b44
Mcrymbo <croco2023@gmail.com>
2023-08-29 20:28:14 +0000

    private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Class for implimenting square object"""
+
+
 class Square:
     """ Class Square that determines the square of by size
     """
@@ -12,24 +15,21 @@ class Square:
         else:
             self.__size = size
 
-        def area(self):
-            """class method that returns square of the object
-            """
-            return (self.__size ** 2)
+    def area(self):
+        """class method that returns square of the object"""
+        return (self.__size ** 2)
 
-        @property
-        def size(self, value):
-            """ Method that returns the size of value
-            """
-            return (self.__size)
+    @property
+    def size(self, value):
+        """ Method that returns the size of value"""
+        return (self.__size)
 
-        @size.setter
-        def size(self, value):
-            """Method for setting the size of square object
-            """
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError(size must be >= 0)
-            else:
-                self.__size = value
+    @size.setter
+    def size(self, value):
+        """Method for setting the size of square object"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError(size must be >= 0)
+        else:
+            self.__size = value
