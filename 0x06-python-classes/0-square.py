e9cddde8eba28f8531d211d196051e17ee77a1e2
Mcrymbo <croco2023@gmail.com>
2023-08-29 19:54:44 +0000

    empty square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Class to implement square """
 class Square:
-    """Class Square is a class that determines a square of a number"""
-    pass
+    """Class Square is a class that determines a square of a number
+    """
