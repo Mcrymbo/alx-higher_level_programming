faf613f8bd3383ae66c2dc08663e6507fbdcfa0d
Mcrymbo <croco2023@gmail.com>
2023-08-30 17:50:16 +0000

    correction

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 import math
-""" Class for margic class """
-
+""" class to store properties of a circle """
 
 class MagicClass:
-    """ class fo margic class """
 
+    """Class that stores the properties of a circle"""
+    
     def __init__(self, radius=0):
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
-    """ Method for calculating area """
+    """ Method that calculates the area of the circle """
     def area(self):
         return ((self.__radius ** 2) * math.pi)
 
-    """ calculates circumference of a square """
+    """ Method that calculates the perimeter of a circle """
     def circumference(self):
-        return (self.__size * math.pi * 2)
+        return (2 * math.pi * self.__radius)
