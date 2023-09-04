49bb44f8172f4c973e2c23debdf8e28d786789da
Mcrymbo <croco2023@gmail.com>
2023-09-04 13:38:44 +0000

    detect deletion

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -3,24 +3,13 @@
 
 
 class Rectangle:
+    """ defines class Rectangle """
 
     def __init__(self, width=0, height=0):
         """ initializes with and height """
 
         self.height = height
         self.width = width
-
-    def area(self):
-        """ calculates area of a rectangle """
-
-        return self.height * self.width
-
-    def perimeter(self):
-        """ calculates perimeter of a rectangle """
-
-        if self.width == 0 or self.height == 0:
-            return 0
-        return (2 * (self.height + self.width))
 
     @property
     def width(self):
@@ -57,6 +46,18 @@ class Rectangle:
         else:
             self.__height = value
         return self.__height
+
+    def area(self):
+        """ calculates area of a rectangle """
+
+        return self.height * self.width
+
+    def perimeter(self):
+        """ calculates perimeter of a rectangle """
+
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (2 * (self.height + self.width))
 
     def __str__(self):
         """ returns rectangle with # """
