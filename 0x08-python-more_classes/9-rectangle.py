f2439111091be04a0564b1e6cad547aa5df53d76
Mcrymbo <croco2023@gmail.com>
2023-09-04 14:13:00 +0000

    square is rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -3,6 +3,7 @@
 
 
 class Rectangle:
+    """ define class Rectangle """
 
     number_of_instances = 0
     print_symbol = "#"
@@ -13,18 +14,6 @@ class Rectangle:
         self.height = height
         self.width = width
         type(self).number_of_instances += 1
-
-    def area(self):
-        """ calculates area of a rectangle """
-
-        return self.height * self.width
-
-    def perimeter(self):
-        """ calculates perimeter of a rectangle """
-
-        if self.width == 0 or self.height == 0:
-            return 0
-        return (2 * (self.height + self.width))
 
     @property
     def width(self):
@@ -61,6 +50,18 @@ class Rectangle:
         else:
             self.__height = value
         return self.__height
+
+    def area(self):
+        """ calculates area of a rectangle """
+
+        return self.height * self.width
+
+    def perimeter(self):
+        """ calculates perimeter of a rectangle """
+
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (2 * (self.height + self.width))
 
     def __str__(self):
         """ returns rectangle with # """
