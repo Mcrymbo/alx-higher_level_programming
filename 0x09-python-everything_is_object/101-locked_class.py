493dab77819aead6e3597cfe7c100f949ac3285c
Mcrymbo <croco2023@gmail.com>
2023-09-05 17:38:20 +0000

    correction

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-""" class LockedClass
-    (prevents the user from dynamically creating new instance)
-"""
-
-
 class LockedClass:
     __slots__ = ["first_name"]
-
-    def __init__(self):
-        """ initializes instance attributes """
