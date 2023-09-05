3cde07ea5e95a633b500ce2202b3d9f0992a2029
Mcrymbo <croco2023@gmail.com>
2023-09-05 17:35:01 +0000

    correction

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-
-
 def magic_string(lst=[]):
-    """ returns the string
-
-    'BestSchool'
-
-    """
-
     lst += ["BestSchool"]
-    return ", ".join(lst)
+    return (", ".join(lst))
