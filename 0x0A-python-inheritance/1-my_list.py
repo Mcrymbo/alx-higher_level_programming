169741f263b9c0046f98dbaa423f58c6ed0aba2f
Mcrymbo <croco2023@gmail.com>
2023-09-11 12:23:26 +0000

    correction

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,6 +8,8 @@ class MyList(list):
     """ inherits from list"""
 
     def print_sorted(self):
+        """ prints a sorted list """
+
         my_list = self[:]
         my_list.sort()
         print(my_list)
