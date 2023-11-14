8210ec5d61026460e3d27f1f1b69828f59867d15
Mcrymbo <croco2023@gmail.com>
2023-11-14 19:55:45 +0300

    all tasks

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+
+Object.keys(dict).map((key, value) => {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
+  return (newDict);
+});
+console.log(newDict);
