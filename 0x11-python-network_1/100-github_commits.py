77ff5bb7f1e5724e2eaa8f173e2988179bb4ed24
Mcrymbo <croco2023@gmail.com>
2023-12-02 00:17:36 +0300

    correction

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,12 +7,17 @@ import sys
 
 
 if __name__ == '__main__':
-    repository = sys.argv[1]
-    owner = sys.argv[2]
+    try:
+        repository = sys.argv[1]
+        owner = sys.argv[2]
 
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repository)
-    r = requests.get(url)
-    json_o = r.json()
-    for i in range(10):
-        print("{}: {}".format(json_o[i].get('sha'),
-                              json_o[i].get('commit').get('author').get('name')))
+        url = 'https://api.github.com/repos/{}/{}/commits'.format(owner,
+                                                                  repository)
+        r = requests.get(url)
+        json_o = r.json()
+        for i in range(10):
+            print("{}: {}".format(json_o[i].get('sha'), json_o[i]
+                                  .get('commit').get('author')
+                                  .get('name')))
+    except Exception:
+        pass
